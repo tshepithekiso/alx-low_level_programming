4ca119e3f0609295091bfd28f712d1c796fac17d
tshepithekiso <tshepangthekiso15@gmail.com>
2023-09-12 14:29:37 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -23,7 +23,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-	printf("%d. ", n);
+	printf("%d, ", n);
 	}
 	}
 	}
@@ -39,7 +39,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-	printf("%d. ", n);
+	printf("%d, ", n);
 	}
 	}
 	}
