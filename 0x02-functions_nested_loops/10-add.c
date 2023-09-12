35efa075665b9a1b0793cd8617a392822ddc2e0a
tshepithekiso <tshepangthekiso15@gmail.com>
2023-09-12 11:25:29 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,8 +2,8 @@
 
 /**
  * add - Adds two values
- * @n: The first number to be computed
- * @n: The second number to be computed
+ * @n1: The first number to be computed
+ * @n2: The second number to be computed
  * Return: Integer value
  */
 
