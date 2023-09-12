3ddf32a0d3ae565dc737fd796881bea6d2952965
tshepithekiso <tshepangthekiso15@gmail.com>
2023-09-12 10:30:02 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * jacl_bauer - Prints every minute of the day of Jack Bauer
- * Retuen: ...
+ * jack_bauer - Prints every minute of the day of Jack Bauer
+ * Return: ...
  */
 
 void jack_bauer(void)
 {
 
 	int a, b, c, d;
+
 	for (a = 0; a <= 2; a++)
 	{
 	for (b = 0; b <= 9; b++)
