7a384c13b25673d9810d7cb3df43adb692533615
tshepithekiso <tshepangthekiso15@gmail.com>
2023-10-05 02:45:52 -0700

    101-mul.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,6 +7,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int main(void);
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
+int main(int argc, char *argv[]);
 
 #endif
