11acf3f19481dd725a7a2281f7e390f16e6e40f7
tshepithekiso <tshepangthekiso15@gmail.com>
2023-10-09 06:46:48 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,29 @@
+#ifndef DOG_HEADER_
+#define DOG_HEADER_
+
+/**
+ * struct dog - dog elements
+ * @name: name of te dog
+ * @age: age of the dog
+ * @owner: owner of te dog
+ *
+ * Description: features of a dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - types of dogs
+ */
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float *age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float *age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
