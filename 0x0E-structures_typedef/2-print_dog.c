c6fd9a33cbd3073e75eadc1ba5ab48f1137d0c29
tshepithekiso <tshepangthekiso15@gmail.com>
2023-10-09 07:14:44 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,7 +13,7 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		printf("NAME: %s\n", (d->name) ? d->name : "(nil)");
-		printf("AGE: %f\n", (d->age) ? d->age :0);
+		printf("AGE: %f\n", (d->age) ? d->age : 0);
 		printf("OWNER: %s\n", (d->owner) ? d->owner : "(nil)");
 	}
 }
