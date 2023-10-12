b55c75e4d3b8837f9b563c2c7848e185daf9c0bb
tshepithekiso <tshepangthekiso15@gmail.com>
2023-10-11 23:19:39 -0700

    2-print_strings.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,10 +20,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		if (!separator)
 			printf("%d", va_arg(list, int));
-		else if (separator && i ==0)
+		else if (separator && i == 0)
 			printf("%d", va_arg(list, int));
 		else
-			printf("%s%d", separator ,va_arg(list, int));
+			printf("%s%d", separator, va_arg(list, int));
 	}
 
 	va_end(list);
