482fbd9d681cbb6db0d5ee1a7f03fe03c5b8e59d
tshepithekiso <tshepangthekiso15@gmail.com>
2023-10-19 00:27:23 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void first(void) __attribute__ ((constructor));
+
+/**
+ * first - first sentence
+ * function is executed
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
